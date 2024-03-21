ec149e85d7dd9fc904f7a966feb7cb20e1c7011e
Zzz-anonymous <firsttry2468@gmail.com>
2024-03-21 21:30:41 +0800

    Create programmeDetailsServlet.java

---
+++ src/java/Servlets/programmeDetailsServlet.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Zy
+ */
+@WebServlet(name = "programmeDetailsServlet", urlPatterns = {"/programmeDetailsServlet"})
+public class programmeDetailsServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+
+}

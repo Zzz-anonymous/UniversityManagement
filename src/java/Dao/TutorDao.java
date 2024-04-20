2e9c6a86ec6df5e1953da41fb4bf6fefd81e1625
Zzz-anonymous <firsttry2468@gmail.com>
2024-04-20 23:38:01 +0800

    add getIndex and getTutorById

--- src/java/Dao/TutorDao.java
+++ src/java/Dao/TutorDao.java
@@ -7,6 +7,7 @@ package Dao;
 import Entity.Tutor;
 import static Utility.Tools.initializeTutors;
 import adt.ListInterface;
+import java.util.Iterator;
 
 /**
  *
@@ -20,6 +21,49 @@ public class TutorDao {
         for (int i = 1; i <= tutor.getTotalNumberOfData(); i++) {
             Tutor t = tutor.getData(i);
             if (t.getName().equals(name)) {
+                return t;
+            }
+        }
+
+        // If no matching programme is found, return null or throw an exception
+        return null;
+    }
+    
+    // check the index number based on id
+    public static int getIndex(String id, ListInterface<Tutor> list) {
+        // Trim the provided ID to remove leading and trailing whitespace
+        String trimmedId = id.trim();
+
+        // Get an iterator for the LinkedList
+        Iterator<Tutor> iterator = list.getIterator();
+
+        // Initialize index counter
+        int index = 1;
+
+        // Iterate over the list
+        while (iterator.hasNext()) {
+            Tutor t = iterator.next();
+            if (t != null) {
+                if (t.getId().equals(trimmedId)) {
+                    // If student ID matches, return the index
+                    return index;
+                }
+            }
+            // Increment index counter
+            index++;
+        }
+
+        // If student ID is not found, return -1
+        return -1;
+    }
+    
+    // get tutor object by id
+    public static Tutor getTutorById(String id) {
+        ListInterface<Tutor> tutor = initializeTutors();
+
+        for (int i = 1; i <= tutor.getTotalNumberOfData(); i++) {
+            Tutor t = tutor.getData(i);
+            if (t.getName().equals(id)) {
                 return t;
             }
         }

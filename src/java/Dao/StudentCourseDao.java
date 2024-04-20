f4740ff66d5ac0b894e3decb761d7e125a04932f
Zzz-anonymous <firsttry2468@gmail.com>
2024-04-20 23:36:45 +0800

    add functions
    getStudentCourseBycId
    filterCourses
    checkValidCourseTime

--- src/java/Dao/StudentCourseDao.java
+++ src/java/Dao/StudentCourseDao.java
@@ -18,6 +18,7 @@ import java.util.Iterator;
 public class StudentCourseDao {
     // Create an LinkedList to store course objects
 
+    private static ListInterface<Course> cList = (ListInterface<Course>) CourseDao.getAllAvailableCourses();
     private static ListInterface<StudentCourse> scList = new LinkedList<>();
     private static ListInterface<Student> sList = StudentDao.getAllStudents();
 
@@ -52,7 +53,7 @@ public class StudentCourseDao {
         return null;
     }
 
-    // get studentCourse list by id
+    // get studentCourse list by student id
     public static LinkedList<StudentCourse> getStudentCourseListById(String id) {
         LinkedList<StudentCourse> studentCourses = new LinkedList<>();
         // Iterate over the list of students to find the one with the matching ID
@@ -66,13 +67,26 @@ public class StudentCourseDao {
         return studentCourses;
     }
 
-    // get Student Course 1 object by id
+    // get Student Course 1 object by student id
     public static Student getStudentById(String id) {
         // Iterate over the list of students to find the one with the matching ID
         for (int i = 1; i <= sList.getTotalNumberOfData(); i++) {
             Student s = sList.getData(i);
             if (s.getId().equals(id)) {
                 return s;
+            }
+        }
+        // Return null if no student with the given ID is found
+        return null;
+    }
+
+    // get Student Course 1 object by course id
+    public static StudentCourse getStudentCourseBycId(String cId) {
+        // Iterate over the list of studentCpurse to find the one with the matching ID
+        for (int i = 1; i <= scList.getTotalNumberOfData(); i++) {
+            StudentCourse sc = scList.getData(i);
+            if (sc.getCourseId().contains(cId)) {
+                return sc;
             }
         }
         // Return null if no student with the given ID is found
@@ -92,7 +106,6 @@ public class StudentCourseDao {
         addCourse(updatedCourses);
     }
 
-   
     // retrieve course status by using courseId
     public static String getStudentCourseStatusById(String courseId) {
         // Iterate over the list of students to find the one with the matching ID
@@ -119,7 +132,7 @@ public class StudentCourseDao {
         return null;
     }
 
-    public static StudentCourse getStudentCourseByIdAndCourseId(String studentId, String courseId) {
+    public static StudentCourse getStudentCourseBysIdAndcId(String studentId, String courseId) {
         // Iterate over the list of student courses to find the one with the matching student ID and course ID
         for (int i = 1; i <= scList.getTotalNumberOfData(); i++) {
             StudentCourse sc = scList.getData(i);
@@ -132,5 +145,55 @@ public class StudentCourseDao {
         return null;
     }
 
+    public static boolean checkValidCourseTime(ListInterface<StudentCourse> list, String studentId, String dayOfWeek, String startTime) {
+        // Iterate over all courses taken by the student
+        for (int i = 1; i <= list.getTotalNumberOfData(); i++) {
+            StudentCourse enrolledCourse = list.getData(i);
+            ListInterface<String> courseIds = enrolledCourse.getCourseId();
+            // Iterate over the course IDs associated with the enrolled course
+            for (int j = 1; j <= courseIds.getTotalNumberOfData(); j++) {
+                String courseId = courseIds.getData(j);
+                // Get the course details of the enrolled course
+                Course enrolledCourseDetails = CourseDao.getCourseById(courseId);
+                // Check if the start time and day of the week of any enrolled course match the proposed course timing
+                if (enrolledCourseDetails != null && enrolledCourseDetails.getDayOfWeek().equals(dayOfWeek)
+                        && enrolledCourseDetails.getStartTime().equals(startTime)) {
+                    return true; // Course time conflict
+                }
+            }
+        }
+        // If no conflicting course is found, the course time is valid
+        return false;
+    }
+
+  
+    public static ListInterface<Course> filterCourses(ListInterface<StudentCourse> scList, ListInterface<Course> cList, String id) {
+        ListInterface<Course> filteredCourses = new LinkedList<>();
+        if (scList != null && cList != null) {
+            for (int k = 1; k <= scList.getTotalNumberOfData(); k++) {
+                StudentCourse sc = scList.getData(k);
+                if (sc.getStudentId().equals(id)) {
+
+                    for (int j = 1; j <= cList.getTotalNumberOfData(); j++) {
+                        Course c = cList.getData(j);
+                        if (sc.getCourseId().contains(c.getId())) {
+                            filteredCourses.add(c);
+                            break;
+
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+        // Check if no courses were found, return null
+        if (filteredCourses.isEmpty()) {
+            return null;
+        }
+
+        return filteredCourses;
+}
 
 }
